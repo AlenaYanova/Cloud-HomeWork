b5397454e97af568605e7de0652f025f44c7fbc4
AlenaYanova <leonayanov@gmail.com>
2017-12-12 17:20:28 +0300

    111

---
+++ Cloud_HW_Yanova.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Cloud_HW_Yanova.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cloud_HW_Yanova.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("61129b6b-6f8f-4851-8e6a-0368234d09e2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Cloud_HW_Yanova.Test/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cloud_HW_Yanova.Models;
+
+namespace Cloud_HW_Yanova.Test
+{
+    [TestClass]
+    public class UnitTesting
+    {
+        [TestMethod]
+        public void HelloTest()
+        {
+            string testName = "Алена";
+
+            string res = VotingController.Hello(testName);
+
+            Assert.AreEqual("Привет, Алена!", res);
+        }
+    }
+}
